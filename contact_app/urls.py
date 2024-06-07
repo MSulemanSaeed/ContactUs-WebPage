6e95cec6a7e2968a3c0e5f22b53f7ac6af0f5d64
MSulemanSaeed <146757569+MSulemanSaeed@users.noreply.github.com>
2024-06-07 15:01:19 +0500

    clean code

--- contact_app/urls.py
+++ contact_app/urls.py
@@ -3,8 +3,4 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('contact/', contact_view, name='contact'),
-    path('about/', about_view, name='about'),
-    path('products/', products_view, name='products'),
-    
 ]
--- contact_app/views.py
+++ contact_app/views.py
@@ -3,28 +3,12 @@ from .models import Contact
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 
-def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/') 
-    else:
-        form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
-
 def home_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/') 
+            return redirect('/#home') 
     else:
         form = ContactForm()
     return render(request, 'home.html', {'form': form})
-
-def about_view(request):
-    return render(request, 'about.html')
-
-def products_view(request):
-    return render(request, 'products.html')
